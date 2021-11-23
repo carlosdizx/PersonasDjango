161395fc6fa72076d6a988eab3f34e67d635a335
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-23 16:28:07 -0500

    Se mejoro la consulta, en dadocaso de que llegue a no enontroar la conulta arroja un 404

--- sap/personas/views.py
+++ sap/personas/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from personas.models import Persona
 
 
 def detallePersona(request, id):
-    persona = Persona.objects.get(pk=id)
+    persona = get_object_or_404(Persona, pk=id)
     return render(request, 'detalle.html', {'persona': persona})
