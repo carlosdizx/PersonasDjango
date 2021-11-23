582bad7242aed3b0cc8577ab0bcb03e4890e0bd1
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-23 09:23:55 -0500

    agregado nuevo metodo con nueva ruta

--- sap/webapp/views.py
+++ sap/webapp/views.py
@@ -4,4 +4,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def bienvenido(request):
-    return HttpResponse(f'Hola mundo {2+2}')
+    return HttpResponse(f'Hola mundo, 2 + 2 = {2 + 2}')
+
+
+def despedirse(request):
+    return HttpResponse(f'Chao mundo, 2 + 2 = {2 + 2}')
