cbfde297ee0bda2c8d43711342ab2332eed2ad02
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-23 16:25:34 -0500

    Mostrando los detalles de la persona indicada por parametro en la url

--- sap/personas/views.py
+++ sap/personas/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from personas.models import Persona
+
+
+def detallePersona(request, id):
+    persona = Persona.objects.get(pk=id)
+    return render(request, 'detalle.html', {'persona': persona})
