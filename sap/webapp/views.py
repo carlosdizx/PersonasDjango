82a58393ae8194ae0ad98df17009afa22bde5295
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-23 15:56:27 -0500

    Se muestra la cantidad de personas que estan registradas en la base de datos

--- sap/webapp/views.py
+++ sap/webapp/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-
 # Create your views here.
+from personas.models import Persona
+
+
 def bienvenido(request):
-    return render(request, 'bienvenido.html')
+    cantidad = Persona.objects.count()
+    return render(request, 'bienvenido.html', {'cantidad': cantidad})
