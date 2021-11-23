550cbaa36f3fce56f9e755b118ddc61376f14517
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-23 11:26:34 -0500

    agregado nuevo metodo para str para mostrar la info del usuario

--- sap/personas/models.py
+++ sap/personas/models.py
@@ -6,3 +6,6 @@ class Persona(models.Model):
     nombre = models.CharField(max_length=255)
     apellido = models.CharField(max_length=255)
     email = models.EmailField(max_length=255)
+
+    def __str__(self):
+        return f'Perosna {self.id}: {self.nombre} {self.apellido}'
